ec060395d1f75b7f1a856041269d426af58a6c2d
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2022-11-25 14:06:25 -0300

    Updated FrmLeitor

--- apBiblioteca_22121_22156/UI/FrmLeitor.cs
+++ apBiblioteca_22121_22156/UI/FrmLeitor.cs
@@ -19,49 +19,83 @@ namespace apBiblioteca_22121_22156.UI
 
         private void btnNovo_Click(object sender, EventArgs e)
         {
-            Leitor leitor = new Leitor(0,"","","","");
-            leitor.NomeLeitor = txtNomeLeitor.Text;
+            Leitor leitor = new Leitor(0,"","","",""); // Insntaciamos um leitor "vazio"
+            // Pegamos os dados dos textboxes referentes ao leitor e nele inserimos esses dados
+            leitor.NomeLeitor     = txtNomeLeitor.Text; 
             leitor.TelefoneLeitor = txtTelefoneLeitor.Text;
-            leitor.EmailLeitor = txtEmailLeitor.Text;
+            leitor.EmailLeitor    = txtEmailLeitor.Text;
             leitor.EnderecoLeitor = txtEnderecoLeitor.Text;
             try
             {
-                LeitorBLL bll = new LeitorBLL(banco, usuario, senha);
-                bll.IncluirLeitor(leitor);
+                LeitorBLL bll = new LeitorBLL(banco, usuario, senha); // Instanciamos um bll
+                bll.IncluirLeitor(leitor); // E incluimos um novo leitor
             }
             catch (Exception erro)
             {
-                MessageBox.Show("Erro: " + erro.Message.ToString());
+                MessageBox.Show("Erro: " + erro.Message.ToString()); // Caso haja algum erro, mostramos ao usuario
             }
         }
 
         private void btnAlterar_Click(object sender, EventArgs e)
         {
+            // Instanciamos um novo leitor pegando os dados referentes a ele dos textboxes, esses valores serao utilizados para atualizar a versao ja existente
             Leitor leitor = new Leitor(int.Parse(txtIdLeitor.Text), txtNomeLeitor.Text, txtTelefoneLeitor.Text, txtEmailLeitor.Text, txtEnderecoLeitor.Text);
             try
             {
-                LeitorBLL bll = new LeitorBLL(banco, usuario, senha);
-                bll.AlterarLeitor(leitor);
+                LeitorBLL bll = new LeitorBLL(banco, usuario, senha); // Instanciamos um bll
+                bll.AlterarLeitor(leitor); // Alteramos os dados de leitor
             }
             catch (Exception erro)
             {
-                MessageBox.Show("Erro: " + erro.Message.ToString());
+                MessageBox.Show("Erro: " + erro.Message.ToString()); // Caso haja algum erro, mostramos ao usuario
             }
         }
 
         private void btnExcluir_Click(object sender, EventArgs e)
         {
-
+            Leitor leitor = new Leitor(Convert.ToInt32(txtIdLeitor.Text), "", "", "", ""); // Instanciamos um novo leitor "zerado", apenas com o Id, que sera usado na operacao de exclusao
+            try
+            {
+                LeitorBLL bll = new LeitorBLL(banco, usuario, senha); // Instanciamos um bll
+                bll.ExcluirLeitor(leitor); // Exlucimos o leitor. O id passado na instancia que sera usado, nao sendo necessario, portanto, passar todos os dados de leitor
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro: " + erro.Message.ToString()); // Caso haja algum erro, mostramos ao usuario
+            }
         }
 
         private void btnExibir_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                LeitorBLL bll = new LeitorBLL(banco, usuario, senha); // Instanciamos um bll
+                dgvLeitor.DataSource = bll.SelecionarListLeitores();  // Pegamos todos os leitores e os adicionamos como fonte de dados para o dgvLeitor
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro: " + erro.Message.ToString()); // Caso haja algum erro, mostramos ao usuario
+            }
         }
 
         private void btnProcurar_Click(object sender, EventArgs e)
         {
-
+            int id = Convert.ToInt32(txtIdLeitor.Text); // Pegamos o id para procurar o leitor
+            Leitor leitor = null; 
+            try
+            {
+                LeitorBLL bll = new LeitorBLL(banco, usuario, senha); // Instanciamos um bll
+                leitor = bll.SelecionarLeitorPorId(id); // Selecionamos o leitor a partir do id passado
+                // E adicionamos aos textboxes os dados correspondentes ao leitor
+                txtNomeLeitor.Text     = leitor.NomeLeitor;
+                txtEmailLeitor.Text    = leitor.EmailLeitor;
+                txtTelefoneLeitor.Text = leitor.TelefoneLeitor;
+                txtEnderecoLeitor.Text = leitor.EnderecoLeitor;
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro: " + erro.Message.ToString()); // Caso haja algum erro, mostramos ao usuario
+            }
         }
 
         public FrmLeitor()
